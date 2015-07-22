01b91feca7fb43d9c2523e1101da79c3ea3d4e8b
ronaldmougoue <mkrchardin@gmail.com>
2015-07-22 16:41:03 +0200

    modification effectue

--- emeraude-erp/src/EPlan/PlanningBundle/Controller/EnseignantController.php
+++ emeraude-erp/src/EPlan/PlanningBundle/Controller/EnseignantController.php
@@ -3,17 +3,37 @@
 namespace EPlan\PlanningBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use EPlan\PlanningBundle\Entity\Enseignant;
 class EnseignantController extends Controller
 {
     public function createEnseignantAction()
     {
-        return $this->render('EPlanPlanningBundle:Enseignant:createEnseignant.html.twig');
+        
+        $ens=new Enseignant();
+        $request=$this->get('request');
+        if($request->getMethod()=='POST'){
+            $ens.setNom($request->get('nom'));
+            $ens.setPrenom($request->get('prenom'));
+            $ens.setTitre($request->get('titre'));
+            $ens.setTel($request->get('tel'));
+            $ens.setEmail($request->get('email'));
+            
+                $em=$this->getDoctrine()->getManager();
+                $em->persist($ens);
+                $em->flush();
+                $effectue=0;
+            
+        }
+        return $this->render('EPlanPlanningBundle:Enseignant:createEnseignant.html.twig',array('ok'=>$effectue));
     }
     
     public function consultEnseignantAction()
     {
-        return $this->render('EPlanPlanningBundle:Enseignant:consultEnseignant.html.twig');
+        $ensRepository= $this->getDoctrine()
+                          ->getManager()
+                          ->getRepository('EPlanPlanningBundle:Enseignant');
+        $listEns= $ensRepository->findAll();
+        return $this->render('EPlanPlanningBundle:Enseignant:consultEnseignant.html.twig',array('listEns'=>$listEns));
     }
     
     public function indexEnseignantAction()
@@ -23,6 +43,8 @@ class EnseignantController extends Controller
     
     public function enregEnseignantAction()
     {
+        
+    
         return $this->render('EPlanPlanningBundle:Enseignant:indexEns.html.twig');
     }
     
